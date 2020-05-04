af1dca0a4ce4263fad6c8dde65413731dbb49bd4
Jude Chinoso <jchinoso@theagromall.com.ng>
2020-05-04 08:52:12 +0100

    Updates

---
+++ precache-manifest.9380cd3f3ecf857b8268410b1049eb80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f081876ee2934733568836767e60e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf6a40ba414939223483",
+    "url": "/static/css/main.2a1db51c.chunk.css"
+  },
+  {
+    "revision": "6d6c5c9648645a7b285f",
+    "url": "/static/js/2.6f75efab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f75efab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6a40ba414939223483",
+    "url": "/static/js/main.434785b5.chunk.js"
+  },
+  {
+    "revision": "310c168f5b4a8bb90b3d",
+    "url": "/static/js/runtime-main.eefa256f.js"
+  }
+]);
